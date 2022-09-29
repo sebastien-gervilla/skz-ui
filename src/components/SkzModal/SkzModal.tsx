27e1aeff88e132b18bfa42bdaa10a7b3301244b5
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-09-29 14:44:34 +0200

    SkzModal

--- src/components/SkzModal/SkzModal.tsx
+++ src/components/SkzModal/SkzModal.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode, useEffect, useRef } from 'react';
+import React, { CSSProperties, ReactNode, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
 
 export interface SkzModalProps {
     open: boolean,
@@ -13,26 +14,49 @@ const SkzModal = (props: SkzModalProps) => {
     const { open, setOpen, body, keepFocus } = props;
 
     useEffect(() => {
+        console.log("This is testing");
+        
         const handleClickOut = (event: MouseEvent) => {
             const modal = modalRef.current;
             if (modal && modal.contains(event.target as Node))
                 setOpen(false);
         }
 
-        keepFocus && document.addEventListener('mousedown', handleClickOut);
+        !keepFocus && document.addEventListener('mousedown', handleClickOut);
 
-        return () => document.removeEventListener('mousedown', handleClickOut);
+        return () => { !keepFocus && document.removeEventListener('mousedown', handleClickOut) };
     }, [modalRef, setOpen, keepFocus]);
 
-    return (
-        <div className="skz-modal_wrapper">
-            <div className="skz-modal" ref={modalRef}>
+    return open && ReactDOM.createPortal(
+        <div className="skz-modal_wrapper" style={_sModalWrapper}>
+            <div className="skz-modal" style={_sModal} ref={modalRef}>
 
-                { open && body }
+                { body }
 
             </div>
         </div>
-    );
+    , document.getElementById("root") as Element)
 };
+
+//#region Styles
+
+const _sModalWrapper: CSSProperties = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#ccc'
+}
+
+const _sModal: CSSProperties = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#fff'
+}
+
+//#endregion
 
 export default SkzModal;
